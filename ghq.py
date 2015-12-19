df75feec19c86c761b82cc7cdee6835e658d7fa1
giginet <giginet.net@gmail.com>
2015-12-19 21:18:51 +0900

    Add skelton project

---
+++ ghq.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import sys
+
+from workflow import Workflow
+
+
+def main(wf):
+    # Get args from Workflow, already in normalised Unicode
+    args = wf.args
+
+    # Do stuff here ...
+
+    # Add an item to Alfred feedback
+    wf.add_item(u'Item title', u'Item subtitle')
+
+    # Send output to Alfred
+    wf.send_feedback()
+
+
+if __name__ == '__main__':
+    wf = Workflow()
+    sys.exit(wf.run(main))
