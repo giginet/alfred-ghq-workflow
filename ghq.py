0003175c9e9df117d460c1faf18725ea334f2ab8
giginet <giginet.net@gmail.com>
2015-12-19 21:59:37 +0900

    Implement ghq list command

--- ghq.py
+++ ghq.py
@@ -1,21 +1,59 @@
 #!/usr/bin/python
 # encoding: utf-8
 
+import os
 import sys
+import commands
 
 from workflow import Workflow
 
 
+class Client(object):
+    def _execute_ghq(self, *args):
+        args = ['/opt/brew/bin/ghq',] + list(args)
+        return commands.getstatusoutput(' '.join(args))
+
+    def is_available(self):
+        status, output = self._execute_ghq()
+        return int(status) == 0
+
+    def fetch_repository_list(self):
+        status, output = self._execute_ghq('list')
+        return output.split('\n')
+
+    def execute_get_repository(self, url):
+        self._execute_ghq('get', url)
+
+    @property
+    def root(self):
+        status, output = self._execute_ghq('root')
+        return output
+
+    def get_path(self, repo_name):
+        return os.path.join(self.root, repo_name)
+
 def main(wf):
     # Get args from Workflow, already in normalised Unicode
     args = wf.args
+    client = Client()
 
-    # Do stuff here ...
+    if client.is_available():
+        if len(args) == 0:
+            pass
+        else:
+            command = args[0]
+            if command == 'get':
+                pass
+            else:
+                query = args[0]
+                repositories = client.fetch_repository_list()
+                filtered = filter(lambda name: query in name, repositories)
+                for repository in filtered:
+                    path = client.get_path(repository)
+                    wf.add_item(repository, u'', arg=path, valid=True)
+    else:
+        wf.add_item(u'ghq is not available', u'')
 
-    # Add an item to Alfred feedback
-    wf.add_item(u'Item title', u'Item subtitle')
-
-    # Send output to Alfred
     wf.send_feedback()
 
 
