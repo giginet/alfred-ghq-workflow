83c5598b756d604a65cf4d0b0d409526b70a42b9
giginet <giginet.net@gmail.com>
2015-12-19 22:46:22 +0900

    Use subprocess instead of outputs

--- ghq.py
+++ ghq.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-import commands
+import subprocess
 
 from workflow import Workflow
 
@@ -11,14 +11,14 @@ from workflow import Workflow
 class Client(object):
     def _execute_ghq(self, *args):
         args = ['ghq',] + list(args)
-        return commands.getstatusoutput(' '.join(args))
+        return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()
 
     def is_available(self):
-        status, output = self._execute_ghq()
-        return int(status) == 0
+        output, err = self._execute_ghq()
+        return not err
 
     def fetch_repository_list(self):
-        status, output = self._execute_ghq('list')
+        output, _ = self._execute_ghq('list')
         return output.split('\n')
 
     def execute_get_repository(self, url):
@@ -26,8 +26,8 @@ class Client(object):
 
     @property
     def root(self):
-        status, output = self._execute_ghq('root')
-        return output
+        output, _ = self._execute_ghq('root')
+        return output.rstrip()
 
     def get_path(self, repo_name):
         return os.path.join(self.root, repo_name)
@@ -49,7 +49,7 @@ def main(wf):
                 filtered = filter(lambda name: query in name, repositories)
                 for repository in filtered:
                     path = client.get_path(repository)
-                    wf.add_item(repository, u'', arg=path, valid=True)
+                    wf.add_item(repository, path, arg=path, valid=True)
     else:
         wf.add_item(u'ghq is not available', u'')
 
